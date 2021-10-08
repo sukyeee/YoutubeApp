0871bf280482e79ef817729236495473613991a1
sk <1724271@donga.ac.kr>
2021-10-09 01:41:30 +0900

    ADD SideVideo

---
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -0,0 +1,54 @@
+import Axios from 'axios'
+import React , {useEffect, useState }from 'react'
+
+function SideVideo() {
+
+    const [sideVideos, setsideVideos] = useState([])
+
+    useEffect(() => {
+        Axios.get('/api/video/getVideos')
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data.videos)
+                    setsideVideos(response.data.videos)
+                } else {
+                    alert('Failed to get Videos')
+                }
+            })
+    }, [])
+
+    const renderSideVideo = sideVideos.map((video, index) => {
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor(video.duration - minutes * 60);
+
+      return <div key={index} style={{ display: 'flex', marginTop: '1rem', padding: '0 2rem' }}>
+        <div style={{ marginRight:'1rem' }}>
+            <a href={`/video/${video._id}`}  style={{ color:'gray' }}>
+                <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+            </a>
+        </div>
+
+        <div style={{ width:'50%' }}>
+            <a href={`/video/${video._id}`} style={{ color:'gray' }}>
+                <span style={{ fontSize: '1rem', color: 'black' }}>{video.title}  </span><br />
+                <span>{video.description}</span><br />
+                <span>{video.writer.name}</span><br />
+                <span>{video.views}</span><br />
+                <span>{minutes} : {seconds}</span><br />
+            </a>
+        </div>
+    </div>
+    })
+
+    return (
+
+      
+        <div>
+            {renderSideVideo}
+
+            
+        </div>
+    )
+}
+
+export default SideVideo
